63544a9bc92465191a00ff5a4359b05f9456493b
starlightctrl <nkubiak455@gmail.com>
2024-12-29 17:45:09 -0600

    fix: Improve XKCD API request handling with .json() method

--- xkcd_api.py
+++ xkcd_api.py
@@ -1,10 +1,11 @@
 import requests
 import random
+import json
 
 def get_comic():
-    latest_comic = requests.get("https://xkcd.com/info.0.json")
+    latest_comic = requests.get("https://xkcd.com/info.0.json").json()
     latest_id = latest_comic['num']
     id = random.randint(1, latest_id)
 
-    comic = requests.get("https://xkcd.com/{id}/info.0.json")
+    comic = requests.get("https://xkcd.com/{id}/info.0.json").json()
     return comic
